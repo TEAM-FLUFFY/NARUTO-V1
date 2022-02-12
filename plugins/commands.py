2929ba7afe9484413c217610d9dacc77556acb35
Fluffy <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-12 16:02:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,7 +39,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎗️𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗔 𝗚𝗥𝗢𝗨𝗣🎗️ :)', url=f'http://t.me/Naruto_ahautofilterbot?startgroup=true')
+            InlineKeyboardButton('🎗️𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗔 𝗚𝗥𝗢𝗨𝗣🎗️', url=f'http://t.me/Naruto_ahautofilterbot?startgroup=true')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
